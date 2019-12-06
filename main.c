a26922b570aef296869fcca4bb858cdc9031bfa1
Venkataramreddy22 <58415046+Venkataramreddy22@users.noreply.github.com>
2019-12-06 12:47:24 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+    int m,p,c,tot;
+    float avg;
+    printf("\n enter the marks in maths,physics,chemistry");
+    scanf("%d%d%d",&m,&p,&c);
+    tot=m+p+c;
+    avg=(float)tot/3;
+    printf("\n total marks: %d",tot);
+    printf("\n average : %f",avg);
+}
+
+
